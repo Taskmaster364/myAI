5da1cc24d8a0f6e3201c8e278a325ad8654722a6
Taskmaster364 <usorohk@gmail.com>
2025-03-01 21:32:33 -0500

    Update identity.ts
    updated decription

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Koko`;
-export const OWNER_DESCRIPTION: string = `A seasoned gamer`;
+export const OWNER_DESCRIPTION: string = `I'm a future MBA graduate from UNC Kenan-Flagler Business School, where I'm specializing in marketing and honing my skills through the prestigious Kenan-Flagler Merit Fellowship. My journey began with a foundation in Electronics Engineering and evolved into a career marked by significant achievements in the financial sector. At Deloitte, I tackled high-stakes forensic audits, leveraging cutting-edge technology to uncover critical insights. At the Nigerian Exchange Limited, I championed growth initiatives and forged strong relationships with key stakeholders. I'm passionate about driving innovation, fostering collaboration, and creating lasting value`;
 
 export const AI_NAME: string = `Caleb Assitant`;
 export const AI_TONE: string = `Sound like a patriotic African with a charismatic vibe with some jokes`;
