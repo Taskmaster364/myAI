ec5b1191e9b6815d0d7e5b59fa5e8b8c47f84d60
Taskmaster364 <usorohk@gmail.com>
2025-03-01 21:30:50 -0500

    Update ui.ts
    changes to the body- clear button

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `Top of the World`;
+export const MESSAGE_PLACEHOLDER: string = `How you dey Abobi, its a greeting in Nigeria`;
 export const FOOTER_MESSAGE: string = `all rights reserved-2025`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `Tap to clear text`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
